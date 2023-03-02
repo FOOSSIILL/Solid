c1a23d7467e8bb6146295cfd74d2449495263f7c
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-03-02 11:08:49 -0500

    feat: InterfaceSegregation.js and LiskovSubstitution.js and OpenClosed.js and pureFunctions.js and SingleResponsibility.js

---
+++ SingleResponsibility.js
@@ -0,0 +1,27 @@
+//SOLID Principles
+
+//Single Responsibility Principle (SRP):
+
+// Antes (no cumple con SRP)
+class UserService {
+    authenticateUser(username, password) {
+        //Código de autenticación de usuario
+    }
+
+    sendEmail(subject, body, to) {
+        // Código para enviar correo electrónico
+    }
+}
+
+// Después (cumple con SRP)
+class AuthenticationService {
+    authenticateUser(username, password) {
+      // código de autenticación de usuario
+    }
+  }
+  
+  class EmailService {
+    sendEmail(subject, body, to) {
+      // código para enviar correo electrónico
+    }
+}
